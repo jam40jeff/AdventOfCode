1cd53cff328083c341ce84e2517a917b419d6077
jam40jeff <jam40jeff@gmail.com>
2021-12-06 10:03:42 -0500

    Clean up 2021 Day 6

--- AdventOfCode2021/Day6.fs
+++ AdventOfCode2021/Day6.fs
@@ -6,16 +6,14 @@ open type AdventOfCodeInput.Input2021
 
 let getInput() = Day6 |> split "," |> Seq.map int |> Seq.toList
 
-let rec processor self daysLeft =
-    if daysLeft < 9 then 1L
-    else 1L + ((Seq.init ((daysLeft - 9)/7 + 1) (fun n -> self (daysLeft - 9 - 7*n))) |> Seq.sum)
+let processor =
+    let rec processor self daysLeft =
+        if daysLeft < 9 then 1L
+        else 1L + ((Seq.init ((daysLeft - 9)/7 + 1) (fun n -> self (daysLeft - 9 - 7*n))) |> Seq.sum)
+    Utils.memoizeRec processor
 
-let calculate processor days v = processor (days + 8 - v)
+let calculate days v = processor (days + 8 - v)
 
-let a() =
-    let processor = Utils.memoizeRec processor
-    getInput() |> Seq.map (calculate processor 80) |> Seq.sum
+let a() = getInput() |> Seq.map (calculate 80) |> Seq.sum
 
-let b() =
-    let processor = Utils.memoizeRec processor
-    getInput() |> Seq.map (calculate processor 256) |> Seq.sum
+let b() = getInput() |> Seq.map (calculate 256) |> Seq.sum
