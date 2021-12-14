188bb1db7e1d5eb2a62d340d779efdb9ec103820
jam40jeff <jam40jeff@gmail.com>
2021-12-13 23:22:01 -0500

    Cleanup.

--- AdventOfCode2021/Day5.fs
+++ AdventOfCode2021/Day5.fs
@@ -5,27 +5,21 @@ open AdventOfCodeCommon
 open AdventOfCodeCommon.InputUtils
 open type AdventOfCodeInput.Input2021
 
-let getStep v1 v2 = if v2 >= v1 then 1 else -1
-
-let getLinePoints ((x1,y1),(x2,y2)) =
-    if x1 = x2 then
-        [y1..(getStep y1 y2)..y2] |> Seq.map (fun y -> (x1,y))
-    else if y1 = y2 then
-        [x1..(getStep x1 x2)..x2] |> Seq.map (fun x -> (x,y1))
+let getPointsAlongLine includeDiagonals ((x1,y1),(x2,y2)) =
+    let getStep v1 v2 = if v2 >= v1 then 1 else -1
+    if x1 = x2 then [y1..(getStep y1 y2)..y2] |> Seq.map (fun y -> (x1,y))
+    else if y1 = y2 then [x1..(getStep x1 x2)..x2] |> Seq.map (fun x -> (x,y1))
+    else if includeDiagonals then Seq.zip [x1..(getStep x1 x2)..x2] [y1..(getStep y1 y2)..y2]
     else Seq.empty
 
-let getLinePointsWithDiagonals ((x1,y1),(x2,y2)) =
-    if x1 = x2 || y1 = y2 then getLinePoints ((x1,y1),(x2,y2))
-    else Seq.zip [x1..(getStep x1 x2)..x2] [y1..(getStep y1 y2)..y2]
-
-let run getLinePoints =
-    let vents =
-        Day5
-        |> getPerLine (fun l ->
-            let points = l |> split " -> " |> Array.map (fun p -> p |> split "," |> Array.map int)
-            match points with
-            | [|[|x1;y1|];[|x2;y2|]|] -> (x1,y1),(x2,y2)
-            | _ -> failwith $"Invalid format for line: %s{l}")
+let run getPointsAlongLine =
+    let getPoints s =
+        let points = s |> split " -> " |> Array.map (fun p -> p |> split "," |> Array.map int)
+        match points with
+        | [|[|x1;y1|];[|x2;y2|]|] -> (x1,y1),(x2,y2)
+        | _ -> failwith $"Invalid format for line: %s{l}"
+    
+    let vents = Day5 |> getPerLine getPoints
     
     let values =
         let maxX = vents |> Seq.map (fun ((x1,_),(x2,_)) -> max x1 x2) |> Seq.max
@@ -33,11 +27,11 @@ let run getLinePoints =
         Array2D.create (maxX + 1) (maxY + 1) 0
     
     let increment (x,y) = values[x,y] <- values[x,y] + 1
-    vents |> Seq.iter (getLinePoints >> Seq.iter increment)
+    vents |> Seq.iter (getPointsAlongLine >> Seq.iter increment)
     
     let isOverlap v = v > 1
     values |> Utils.flattenArray2D |> Seq.filter isOverlap |> Seq.length
 
-let a() = run getLinePoints
+let a() = run (getPointsAlongLine false)
 
-let b() = run getLinePointsWithDiagonals
+let b() = run (getPointsAlongLine true)
