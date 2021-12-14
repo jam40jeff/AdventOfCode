b911d9374f4515ead125b4e92f84b1328e3acf6b
jam40jeff <jam40jeff@gmail.com>
2021-12-13 23:24:07 -0500

    Cleanup.

--- AdventOfCode2021/Day5.fs
+++ AdventOfCode2021/Day5.fs
@@ -5,14 +5,7 @@ open AdventOfCodeCommon
 open AdventOfCodeCommon.InputUtils
 open type AdventOfCodeInput.Input2021
 
-let getPointsAlongLine includeDiagonals ((x1,y1),(x2,y2)) =
-    let getStep v1 v2 = if v2 >= v1 then 1 else -1
-    if x1 = x2 then [y1..(getStep y1 y2)..y2] |> Seq.map (fun y -> (x1,y))
-    else if y1 = y2 then [x1..(getStep x1 x2)..x2] |> Seq.map (fun x -> (x,y1))
-    else if includeDiagonals then Seq.zip [x1..(getStep x1 x2)..x2] [y1..(getStep y1 y2)..y2]
-    else Seq.empty
-
-let run getPointsAlongLine =
+let run includeDiagonals =
     let getPoints s =
         let points = s |> split " -> " |> Array.map (fun p -> p |> split "," |> Array.map int)
         match points with
@@ -26,12 +19,19 @@ let run getPointsAlongLine =
         let maxY = vents |> Seq.map (fun ((_,y1),(_,y2)) -> max y1 y2) |> Seq.max
         Array2D.create (maxX + 1) (maxY + 1) 0
     
+    let getPointsAlongLine ((x1,y1),(x2,y2)) =
+        let getStep v1 v2 = if v2 >= v1 then 1 else -1
+        if x1 = x2 then [y1..(getStep y1 y2)..y2] |> Seq.map (fun y -> (x1,y))
+        else if y1 = y2 then [x1..(getStep x1 x2)..x2] |> Seq.map (fun x -> (x,y1))
+        else if includeDiagonals then Seq.zip [x1..(getStep x1 x2)..x2] [y1..(getStep y1 y2)..y2]
+        else Seq.empty
+
     let increment (x,y) = values[x,y] <- values[x,y] + 1
     vents |> Seq.iter (getPointsAlongLine >> Seq.iter increment)
     
     let isOverlap v = v > 1
     values |> Utils.flattenArray2D |> Seq.filter isOverlap |> Seq.length
 
-let a() = run (getPointsAlongLine false)
+let a() = run false
 
-let b() = run (getPointsAlongLine true)
+let b() = run true
